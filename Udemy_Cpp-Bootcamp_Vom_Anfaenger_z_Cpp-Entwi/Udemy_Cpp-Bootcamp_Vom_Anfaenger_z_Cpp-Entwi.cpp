be1ba4dbf6609a4c95d6664dd1485b25db8d6827
Fabian.Ferley <Fabian.Ferley@protonmail.com>
2019-07-09 14:58:24 +0200

    Arbeitsende

--- Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
+++ Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
@@ -1,20 +1,58 @@
-// Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
-
 #include <iostream>
+#include <string.h>
+using namespace std;
 
-int main()
+bool reset = 0;
+string eingabe(string message)
 {
-    std::cout << "Hello World!\n";
+	string ergebnis = "";
+	cout << message;
+	cin >> ergebnis;
+	return ergebnis;
 }
 
-// Programm ausführen: STRG+F5 oder "Debuggen" > Menü "Ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
+int kostenberechnen(string ziel) 
+{
+	float preis = 0;
+	if (ziel=="hamburg"||ziel=="Hamburg")
+	{
+		preis = 50;
+	}
+	else if (ziel == "Hannover"|| ziel == "hannover"|| ziel == "Hanover"|| ziel == "hanover")
+	{
+		preis = 70;
+	}
+	/*else					//geplant ist die main Funktion neu zu starten
+	{
+		reset = true;
+		int main();
+		cout << "Reset aktiviert!";
+	}*/ 
 
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
+	return preis;
+
+}
+
+
+int main() 
+{
+	if (reset == true) 
+	{
+		cout << "\nDIE EINGABE WAR FALSCH! VON VORNE!!!\n";
+		reset = 0;
+	}
+	string ziel = eingabe("Wohin wollen Sie fahren\n");
+	int kosten = kostenberechnen(ziel);
+	string bahncard = eingabe("\nHaben Sie eine Bahncard\n");
+	cout << "\nDer Preis fuer ihr Ziel " << ziel << " betraegt " << kosten << " Euro!\n";
+	
+
+	
+	
+}		
+
+/*mit Card
+ohne/mit rückfahrt
+kostet $$
+eingezahlen
+Es fehlen noch $$*/
