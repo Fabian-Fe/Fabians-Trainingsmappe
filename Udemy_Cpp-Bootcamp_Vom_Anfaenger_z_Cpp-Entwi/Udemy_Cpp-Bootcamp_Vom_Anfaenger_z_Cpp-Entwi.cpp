44e0806d5262869129a26fd374ef3310f477e3e6
Fabian.Ferley <Fabian.Ferley@protonmail.com>
2019-07-10 14:25:28 +0200

    KONFLIKTREICHER COMMIT

--- Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
+++ Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
@@ -2,48 +2,21 @@
 #include <string.h>
 using namespace std;
 
-bool reset = 0;
-string eingabe(string message)
+void swap(int* a, int* b)
 {
-	string ergebnis = "";
-	cout << message;
-	cin >> ergebnis;
-	return ergebnis;
+	int swap = *a;
+	*a = *b;
+	*b = swap;
 }
-// sadsadsasd
-int kostenberechnen(string ziel) 
-{
-	float preis = 0;
-	if (ziel=="hamburg"||ziel=="Hamburg")
-	{
-		preis = 50;
-	}
-	else if (ziel == "Hannover"|| ziel == "hannover"|| ziel == "Hanover"|| ziel == "hanover")
-	{
-		preis = 70;
-	}
-	
-
-	return preis;
-
-}
-
 
 int main() 
 {
-	
-	string ziel = eingabe("Wohin wollen Sie fahren\n");
-	int kosten = kostenberechnen(ziel);
-	string bahncard = eingabe("\nHaben Sie eine Bahncard\n");
-	cout << "\nDer Preis fuer ihr Ziel " << ziel << " betraegt " << kosten << " Euro!\n";
-	
+	int a, b;
+	a = 1;
+	b = 2;
+	swap(a,b);
+	cout << "\n a= " << a << " b=" << b<<"\n";
 
-	
+
 	
 }		
-
-/*mit Card
-ohne/mit rückfahrt
-kostet $$
-eingezahlen
-Es fehlen noch $$*/
