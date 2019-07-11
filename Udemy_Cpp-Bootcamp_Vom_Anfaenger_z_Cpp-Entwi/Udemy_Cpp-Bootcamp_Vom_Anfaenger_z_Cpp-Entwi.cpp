ec85af7004f30496a0fca0e6405c124a1b855f04
Fabian.Ferley <Fabian.Ferley@protonmail.com>
2019-07-11 15:03:30 +0200

    arbeitsende

--- Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
+++ Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
@@ -1,73 +1,33 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
+string helloW = "Hello World!";
 
-
-float eingabeFloat(string message, float ausstehend)
+string eingabe(string message)
 {
-	float ergebnis = 0;
-	cout << message << ausstehend << endl;
-	cin >> ergebnis;
-	return ergebnis;
-}
+	string eingabe;
 
-string eingabeStr(string message)
-{
-	string ergebnis = "";
-	cout << message;
-	cin >> ergebnis;
-	return ergebnis;
-}
-
-int kostenberechnen(string antwort)
-{
-
-	if (antwort == "hamburg" || antwort == "Hamburg" || antwort == "HAMBURG")
-		return 50;
-	else if (antwort == "Hannover" || antwort == "hannover" || antwort == "Hanover" || antwort == "hanover" || antwort == "HANNOVER")
-		return 70;
-
-
-}
-
-float rabattisierung(string antwortBC, float kosten)
-{
-
-	if (antwortBC == "25")
+	do
 	{
-		return (kosten * .75);
-	}
-	else if (antwortBC == "50")
-	{
-		return (kosten * .5);
-	}
-	else
-	{
-		return kosten;
-	}
+		cout << message << endl;;
+		cin >> eingabe;
+	} while (eingabe == "");
+	
+	return eingabe;
 }
 
-void schuldenAbgleich(float eingezahlt, float gesamtpreis)			//rabattierterBetrag geht zu gesamtpreis über
+int main() 
 {
-	while (eingezahlt < gesamtpreis)
-	{
-		eingezahlt += eingabeFloat("Bitte jetzt noch zahlen: ", (gesamtpreis - eingezahlt));
-	}
+	const int size = helloW.length();;
+	//string wort = eingabe("Prüfung ob Wort Isogramm ist: ");
+	
+	char char_array[size];
+	
+	//strcpy(char_array, wort);
+	cout << endl<< wlength;
+
+
 
 }
 
-
-int main()
-{
-	string ziel = eingabeStr("Wohin wollen Sie fahren\n"); //ziel muss für später deklariert gebraucht.
-														//gibt eingabeergebnis zurück
-	int kosten = kostenberechnen(ziel);				//gibt an ziel gekoppelten preis zurück
-	string bahncard = eingabeStr("\nHaben Sie die Bahncard 25 oder 50\n");
-	float rabattierterBetrag = rabattisierung(bahncard, kosten);
-
-	cout << "Regulaere Kosten sind " << kosten << " Euro, aber durch die BahnCard bleiben " << rabattierterBetrag << endl;
-	schuldenAbgleich(0, rabattierterBetrag);
-
-	cout << "\nVielen Dank fuer die Zahlung von " << rabattierterBetrag << " Euro!";
-}
 
