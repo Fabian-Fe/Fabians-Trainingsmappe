95d93dfea3526ba1cae9847b687a72aca64b0c6e
Fabian.Ferley <Fabian.Ferley@protonmail.com>
2019-07-15 14:03:55 +0200

    random array

--- Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
+++ Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
@@ -1,33 +1,44 @@
 #include <iostream>
 #include <string.h>
+#include <cstdlib>
+#include <ctime>	//to manipulate rand
 using namespace std;
-string helloW = "Hello World!";
 
-string eingabe(string message)
+int main()
 {
-	string eingabe;
+	srand(time(nullptr)); // use current time as seed for random generator
+	int random_variable = rand();
+	cout << "Random value on [0 " << RAND_MAX << "]: "
+		<< random_variable << '\n';
 
-	do
-	{
-		cout << message << endl;;
-		cin >> eingabe;
-	} while (eingabe == "");
-	
-	return eingabe;
+	// roll a 6-sided die 20 times
+	for (int n = 0; n != 20; ++n) {
+		int x = 7;
+		while (x > 6)
+			x = 1 + rand() / ((RAND_MAX + 1u) / 6);  // Note: 1+rand()%6 is biased
+		cout << x << ' ';
+	}
+}
+
+
+/*
+void myRandom(int &r) 
+{
+	srand(time(NULL));
+	r = rand(); // rand() return a number between ​0​ and RAND_MAX
+	//cout << random_number;
 }
 
 int main() 
 {
-	const int size = helloW.length();;
-	//string wort = eingabe("Prüfung ob Wort Isogramm ist: ");
+	int r;
+	int arrayA[] = { 0,1,2,3,4,5 };
+	int i;
+	for (i = 0; i < 6; i++) {
+		myRandom(r);
+		cout << arrayA[r]<< endl;
+	}
 	
-	char char_array[size];
-	
-	//strcpy(char_array, wort);
-	cout << endl<< wlength;
-
-
-
-}
+}*/
 
 
