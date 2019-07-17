ed46414e1a399f4c222e2daf9048f07a9eb7d9d6
Fabian.Ferley <Fabian.Ferley@protonmail.com>
2019-07-17 14:01:03 +0200

    swap programm

--- Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
+++ Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
@@ -1,33 +1,18 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
-string helloW = "Hello World!";
 
-string eingabe(string message)
-{
-	string eingabe;
+void swap(int,int);
 
-	do
-	{
-		cout << message << endl;;
-		cin >> eingabe;
-	} while (eingabe == "");
-	
-	return eingabe;
-}
 
 int main() 
 {
-	const int size = helloW.length();;
-	//string wort = eingabe("Prüfung ob Wort Isogramm ist: ");
-	
-	char char_array[size];
-	
-	//strcpy(char_array, wort);
-	cout << endl<< wlength;
-
-
-
+	int x = 0;
+	rek(x);
 }
 
-
+void swap(int a,int b)
+{
+	a = (a < b ? a : b) 
+		
+}
