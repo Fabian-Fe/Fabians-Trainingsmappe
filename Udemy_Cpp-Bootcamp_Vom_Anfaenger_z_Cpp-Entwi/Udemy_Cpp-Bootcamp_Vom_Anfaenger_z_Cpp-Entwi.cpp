4db71d66ccbf91cd394eeea3d74e49643166835e
Fabian.Ferley <Fabian.Ferley@protonmail.com>
2019-07-18 11:52:13 +0200

    Lotto sorting

--- Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
+++ Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
@@ -1,34 +1,60 @@
 #include <iostream>
 #include <string.h>
+#include <array>
+#include <cstdlib> 
+#include <ctime> 
+
 using namespace std;
 
-void swap(int,int);
+void swap(int &a, int &b)
+{
+	int swapspace = a;
+	a = b;
+	b = swapspace;
+}
+
+
+int random(int r)
+{
+	srand(time(NULL));
+	return((rand() % (r - 1)) + 1);
+
+}
+
+void randomizeLotto(int* lotto)
+{
+	int i, ziehungen = 5; //random(5)
+	for (i = 0; i < ziehungen; ++i)
+		lotto[i] = random(49);
+}
 
 
 int main() 
 {
-	int lotto[] = { 42,3,49,12 };
-	int posListe[4];
+	int lotto[6];
+	randomizeLotto(lotto);
+	const int length = (sizeof(lotto) / sizeof(*lotto))-1;
+	int posListe[length];
 	//outer loop
-
-		// for loop with i
-	if (lotto[0] < lotto[1])	
-		int smallest = i
-	// end loop, smalles of round determined
-		posListe [j]= smallest
-		//outer loop
-
-		//rearrangeloop
-		for (i = 0; i < length; i++)
+	int i;
+	int smallest = lotto[0];
+	for (i = 0; i < length; i++)
+	{
+		int j;
+		for (j = i; j < length; j++)
 		{
-			swap (lotto[i],posListe)
+			if (lotto[i] > lotto[j]){
+				int smallest = i;			
+				cout << lotto[i] << " ist groesser als " << lotto[j]
+				<< " also Tausch!"<< endl;
+				swap(lotto[i], lotto[j]);
+			}
 		}
-		swap
-		
-}
 
-void swap(int a,int b)
-{
+	}
+	cout << endl << "Lotto sortiert: ";
+	for (i = 0; i < length; i++)
+		cout << lotto[i] << ", ";
+	cout << endl;
 	
-		
 }
