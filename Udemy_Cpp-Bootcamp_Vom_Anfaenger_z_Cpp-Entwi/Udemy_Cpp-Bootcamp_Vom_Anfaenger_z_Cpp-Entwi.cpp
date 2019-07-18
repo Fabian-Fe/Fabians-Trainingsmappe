72db05946f4969b7ce28e8bc212336ac30d65764
Fabian.Ferley <Fabian.Ferley@protonmail.com>
2019-07-18 14:29:52 +0200

    finished random lotto sorting algorithm

--- Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
+++ Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
@@ -12,24 +12,31 @@ void swap(int &a, int &b)
 	a = b;
 	b = swapspace;
 }
-
-
-int random(int r)
+void doppelte_raus(int* lotto)
+{
+	int i;
+	for (i=0;i<5;++i){
+		while(lotto[i]== lotto[i+1]|| lotto[i] == lotto[i+2] || lotto[i] == lotto[i+3] || lotto[i] == lotto[i+4] || lotto[i] == lotto[i+5])
+		{
+			cout << "while(lotto[" << i << "] gleich!";
+			lotto[i]= rand() % (49 - 1) + 1;
+		}
+	}
+}
+//generiert irgendwie randSeed zu anfang
+void runRandom()
 {
 	int ende = time(NULL);
 	for (int i = 0; i < ende % 255; i++) 
-	{
-		return (rand() % (r-1) + 1);
-	}
-}
-
-void randomizeLotto(int* lotto,int length)
+		rand();
+}				
+//füllt lotto[10] mit 10 zufälligen zahlen zw. 1 u 49
+void randomizeLotto(int* lotto,int length,int r=49)
 {
 	int i;
 	for (i = 0; i < length; ++i)
-		lotto[i] = random(49);
+		lotto[i] = rand() % (r-1) + 1;
 }
-
 void printStatus(string message,int* lotto)
 {
 	cout << endl << message;
@@ -38,26 +45,31 @@ void printStatus(string message,int* lotto)
 		cout << lotto[i] << ", ";
 	cout << endl;
 }
-
-int main() 
+void sizeComparison(int* lotto, int length)
 {
-	int lotto[10];
-	const int length = (sizeof(lotto) / sizeof(*lotto))-1;
-	randomizeLotto(lotto, length);
-	printStatus("Lotto UNsortiert: ", lotto);
 	int  i;
 	for (i = 0; i < length; i++)
 	{
 		int j;
 		for (j = i; j < length; j++)
 		{
-			if (lotto[i] > lotto[j]){
-			//	cout << lotto[i] << " ist groesser als " << lotto[j]
-			//	<< " also Tausch!"<< endl;
+			if (lotto[i] > lotto[j]) {
+				//	cout << lotto[i] << " ist groesser als " << lotto[j]
+				//	<< " also Tausch!"<< endl;
 				swap(lotto[i], lotto[j]);
 			}
 		}
 
 	}
+}
+int main() 
+{
+	runRandom();
+	int lotto[6];
+	const int length = (sizeof(lotto) / sizeof(*lotto));
+	randomizeLotto(lotto, length);
+	doppelte_raus(lotto);
+	printStatus("Lotto UNsortiert: ", lotto);
+	sizeComparison(lotto, length);
 	printStatus("Lotto sortiert:   ", lotto);	
 }
