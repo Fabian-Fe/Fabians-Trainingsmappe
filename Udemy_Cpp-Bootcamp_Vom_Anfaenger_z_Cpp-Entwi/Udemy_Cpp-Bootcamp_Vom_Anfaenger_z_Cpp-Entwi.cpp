6cad1f18e7aa4e33bf1cb7ff55377d621c0f1cd9
Fabian.Ferley <Fabian.Ferley@protonmail.com>
2019-07-29 10:47:26 +0200

    Initial Commit

--- Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
+++ Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
@@ -1,34 +1,15 @@
 #include <iostream>
+#include <stdio.h>
 #include <string.h>
 using namespace std;
 
-void swap(int,int);
 
 
-int main() 
+void main() 
 {
-	int lotto[] = { 42,3,49,12 };
-	int posListe[4];
-	//outer loop
-
-		// for loop with i
-	if (lotto[0] < lotto[1])	
-		int smallest = i
-	// end loop, smalles of round determined
-		posListe [j]= smallest
-		//outer loop
-
-		//rearrangeloop
-		for (i = 0; i < length; i++)
-		{
-			swap (lotto[i],posListe)
-		}
-		swap
-		
-}
-
-void swap(int a,int b)
-{
-	
-		
+	string firstname;
+	printf("Type your first name: ");
+	cin>> firstname;
+	for (int x = firstname.length()-1; x >= 0; x--)
+		cout<<firstname.at(x);
 }
