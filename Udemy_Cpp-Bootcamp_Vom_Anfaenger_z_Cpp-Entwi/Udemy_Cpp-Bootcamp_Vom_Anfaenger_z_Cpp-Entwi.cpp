fcd4d88dbcc6d3d79d0f9f0cf2def359e8a5f48d
Fabian.Ferley <Fabian.Ferley@protonmail.com>
2019-07-31 09:43:37 +0200

    while ungültiger Int, infin-loop Dilemma

--- Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
+++ Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 #include <string.h>
+#include <array>
 using namespace std;
 
-void swap(int,int);
+int average(double* x, int length)
+{
+	double summe=0; //einfach nur ein zaehler
+	for (int i = 0; i < length; i++)
+		summe += x[i];
+	//cout << "length ist: " << length<< endl;
+	return (summe/length);
+}
 
 
 int main() 
 {
-	int lotto[] = { 42,3,49,12 };
-	int posListe[4];
-	//outer loop
-
-		// for loop with i
-	if (lotto[0] < lotto[1])	
-		int smallest = i
-	// end loop, smalles of round determined
-		posListe [j]= smallest
-		//outer loop
-
-		//rearrangeloop
-		for (i = 0; i < length; i++)
-		{
-			swap (lotto[i],posListe)
+		int c, length=0;
+		double x[255];
+		cout << "Wie lang? (Max 255)" << endl;
+		while (length < 1 || length >255 || length == NULL) 
+		{				//ERROR, wenn cin kein int ist z.B. qwert oder .99
+			cin >> length;
+			cout << "\n length: "<< length<< endl;
+			cin.clear();
 		}
-		swap
-		
+
+		for (c=0; c < length; c++)
+		{
+			cout << "\nWert " << (c + 1) << ":";
+			cin >> x[c];
+		}
+		cout<< "\nDer Durchschnitt ist: " << average(x, length);
+	
 }
 
-void swap(int a,int b)
-{
-	
-		
-}
