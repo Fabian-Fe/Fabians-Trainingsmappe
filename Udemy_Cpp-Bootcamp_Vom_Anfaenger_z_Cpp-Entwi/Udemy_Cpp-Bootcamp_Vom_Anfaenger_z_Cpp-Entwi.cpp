b2fa3c20ce99ce4794392681be1eb7161dcc34ec
Fabian.Ferley <Fabian.Ferley@protonmail.com>
2019-08-08 10:02:07 +0200

    new master

--- Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
+++ Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
@@ -1,36 +1,44 @@
 #include <iostream>
-#include <string.h>
-#include <array>
-using namespace std;
 
-int average(double* x, int length)
+
+class Reifen 
 {
-	double summe=0; //einfach nur ein zaehler
-	for (int i = 0; i < length; i++)
-		summe += x[i];
-	//cout << "length ist: " << length<< endl;
-	return (summe/length);
+	double luftdruck;
+	double profil;
+public:
+	Reifen(double p = 2.2, double l = 1.6)
+	{
+		profil = p;
+		luftdruck = l;
+	}
+	void ausgeben();
+	void set_profil(double);
+};
+
+void Reifen::ausgeben() 
+{
+	std::cout << "Luftdruck: " << this->luftdruck<< " bar || Profilstaerke: "<<this->profil<< " mm\n";
 }
 
-
-int main() 
+void Reifen::set_profil(double newprofil)
 {
-		int c, length=0;
-		double x[255];
-		cout << "Wie lang? (Max 255)" << endl;
-		while (length < 1 || length >255 || length == NULL) 
-		{				//ERROR, wenn cin kein int ist z.B. qwert oder .99
-			cin >> length;
-			cout << "\n length: "<< length<< endl;
-			cin.clear();
-		}
-
-		for (c=0; c < length; c++)
-		{
-			cout << "\nWert " << (c + 1) << ":";
-			cin >> x[c];
-		}
-		cout<< "\nDer Durchschnitt ist: " << average(x, length);
-	
+	this->profil = newprofil;
 }
 
+class Auto
+{
+	std::string marke = "NoBrand";
+	Reifen reifen[4];
+public:
+	Auto(std::string m)
+	{		
+		marke=m;
+	}
+
+};
+
+void main()
+{
+	Auto autoEins("Ford");
+	reifen[3].ausgeben();
+}
