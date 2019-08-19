950612d321b76229b0ebea7de9cc93a8b4fa1574
Fabian.Ferley <Fabian.Ferley@protonmail.com>
2019-08-19 11:02:00 +0200

    Initial commit

--- Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
+++ Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
@@ -1,44 +1,77 @@
 #include <iostream>
+#include <vector>
+#include <cstring>
 
-
-class Reifen 
+class Flight
 {
-	double luftdruck;
-	double profil;
 public:
-	Reifen(double p = 2.2, double l = 1.6)
-	{
-		profil = p;
-		luftdruck = l;
-	}
+	Flight(int numb); //FlugNr.
+	~Flight();
 	void ausgeben();
-	void set_profil(double);
+	friend class Passenger; 
+	std::vector<Passenger> passList;	//Liste fuer Passagiere auf dem Flug
+	
+private:
+	int number;
 };
 
-void Reifen::ausgeben() 
+
+//Constructor
+Flight::Flight(int n):number(n)
 {
-	std::cout << "Luftdruck: " << this->luftdruck<< " bar || Profilstaerke: "<<this->profil<< " mm\n";
+	std::cout << "Flug# "<< number <<" wurde erstellt!\n";
+}
+//Desctructor
+Flight::~Flight()
+{
+	std::cout << "Flug# " << number << " wurde geloescht!\n";
 }
 
-void Reifen::set_profil(double newprofil)
+//Function
+void Flight::ausgeben() 
 {
-	this->profil = newprofil;
+	std::cout << "Flug# " << number << "s Passagiere: ";
+	//for (const Passenger &p : passList)
+
+	for (int p = 0; p < 1; p++)			//passList.length() doesnt work for size
+		std::cout << passList.at(p) << std::endl; 
 }
 
-class Auto
+class Passenger
 {
-	std::string marke = "NoBrand";
-	Reifen reifen[4];
 public:
-	Auto(std::string m)
-	{		
-		marke=m;
-	}
+	Passenger(std::string name);			
+	~Passenger();
+	//Flight PassengersFlight;		//soll spaeter Liste mit Fluegen sein
+	void add_2flight(Flight flght); //fuegt einen Flug zu des Passagiers Liste an Fluegen dazu
+	friend class Flight;
 
+private:
+	std::string name;
 };
 
-void main()
+//Konstruktor
+Passenger::Passenger(std::string n) :name(n)
 {
-	Auto autoEins("Ford");
-	reifen[3].ausgeben();
+	std::cout << "Passagierin " << name <<" wurde erstellt!\n";
+}
+
+//Destruktor
+Passenger::~Passenger()
+{
+	std::cout << "Passagierin " << name << " wurde geloescht!\n";
+}	   
+
+void Passenger::add_2flight(Flight flght)		//
+{
+	flght.passList.push_back(*this);
+}
+
+int main()
+{
+	Flight Flug3789(3789);
+	Passenger Pascal("Pascal");
+	Pascal.add_2flight(Flug3789);
+	Flug3789.ausgeben();
+
 }
