8c776fc6ad10f0a71064b0c64ff7829bfcebfbc1
Fabian.Ferley <Fabian.Ferley@protonmail.com>
2019-08-19 16:07:51 +0200

    update

--- Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
+++ Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi/Udemy_Cpp-Bootcamp_Vom_Anfaenger_z_Cpp-Entwi.cpp
@@ -2,25 +2,28 @@
 #include <vector>
 #include <cstring>
 
+
 class Flight
 {
 public:
 	Flight(int numb); //FlugNr.
-	~Flight();
 	void ausgeben();
 	friend class Passenger; 
 	std::vector<Passenger> passList;	//Liste fuer Passagiere auf dem Flug
-	
+	~Flight();
 private:
 	int number;
 };
 
 
 //Constructor
-Flight::Flight(int n):number(n)
+Flight::Flight(int n):number(n) 
 {
 	std::cout << "Flug# "<< number <<" wurde erstellt!\n";
+	//C26495	Die Variable "Flight::uebergabe" ist nicht initialisiert.
+	//Eine Membervariable muss immer initialisiert werden(type.6)
 }
+
 //Desctructor
 Flight::~Flight()
 {
@@ -28,26 +31,26 @@ Flight::~Flight()
 }
 
 //Function
-void Flight::ausgeben() 
+void Flight::ausgeben()
 {
 	std::cout << "Flug# " << number << "s Passagiere: ";
-	//for (const Passenger &p : passList)
-
-	for (int p = 0; p < 1; p++)			//passList.length() doesnt work for size
-		std::cout << passList.at(p) << std::endl; 
+	std::cout << passList.at(0).name << std::endl; 	//C2027	 Verwendung des undefinierten Typs "Passenger"	
 }
 
 class Passenger
 {
 public:
 	Passenger(std::string name);			
-	~Passenger();
-	//Flight PassengersFlight;		//soll spaeter Liste mit Fluegen sein
-	void add_2flight(Flight flght); //fuegt einen Flug zu des Passagiers Liste an Fluegen dazu
+	//Flight PassengersFlight;		//soll spaeter Liste mit mehreren Fluegen sein
+	void add_2flight(Flight& flght);//fuegt einen Passagiers an PassagierListe von 1 Flug dazu
 	friend class Flight;
+	friend void Flight::ausgeben();
+	~Passenger();
 
-private:
+
+//private:				
 	std::string name;
+
 };
 
 //Konstruktor
@@ -56,13 +59,14 @@ Passenger::Passenger(std::string n) :name(n)
 	std::cout << "Passagierin " << name <<" wurde erstellt!\n";
 }
 
+
 //Destruktor
 Passenger::~Passenger()
 {
 	std::cout << "Passagierin " << name << " wurde geloescht!\n";
 }	   
 
-void Passenger::add_2flight(Flight flght)		//
+void Passenger::add_2flight(Flight& flght)		//
 {
 	flght.passList.push_back(*this);
 }
@@ -72,6 +76,6 @@ int main()
 	Flight Flug3789(3789);
 	Passenger Pascal("Pascal");
 	Pascal.add_2flight(Flug3789);
-	Flug3789.ausgeben();
-
+	std::cout << Flug3789.passList.at(0).name;
+	Flug3789.ausgeben(); 
 }
